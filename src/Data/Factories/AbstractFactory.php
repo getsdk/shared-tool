2152eb4491ccd575143f58f4306818306fd3899f
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-11-22 14:53:45 +0300

    Create AbstractFactory.php

---
+++ src/Data/Factories/AbstractFactory.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace GetSDK\Shared\Tool\Data\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory as AbstractLaravelFactory;
+
+/**
+ * AbstractFactory
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+abstract class AbstractFactory extends AbstractLaravelFactory
+{
+
+}
