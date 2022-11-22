17d69053e20193c4e913de5ddea9ddfd19af45ae
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-11-22 15:42:18 +0300

    Create HasFactoryTrait.php

---
+++ src/Data/Factories/Traits/HasFactoryTrait.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace GetSDK\Shared\Tool\Data\Factories\Traits;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory as LaravelHasFactoryTrait;
+
+/**
+ * HasFactoryTrait
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+trait HasFactoryTrait
+{
+    use LaravelHasFactoryTrait;
+}
