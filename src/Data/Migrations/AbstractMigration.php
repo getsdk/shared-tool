008b1e0dd2e3036da765888ef098ed67ac5de2e5
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-11-22 14:53:47 +0300

    Create AbstractMigration.php

---
+++ src/Data/Migrations/AbstractMigration.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace GetSDK\Shared\Tool\Data\Migrations;
+
+use Illuminate\Database\Migrations\Migration as AbstractLaravelMigration;
+
+/**
+ * AbstractMigration
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+abstract class AbstractMigration extends AbstractLaravelMigration
+{
+
+}
