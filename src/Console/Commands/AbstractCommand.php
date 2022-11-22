56570413d2a3db1ac78aa9754f05e0cfbc94dba2
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-11-22 14:53:43 +0300

    Create AbstractCommand.php

---
+++ src/Console/Commands/AbstractCommand.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace GetSDK\Shared\Tool\Console\Commands;
+
+use Illuminate\Console\Command as LaravelCommand;
+
+/**
+ * AbstractCommand
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+abstract class AbstractCommand extends LaravelCommand
+{
+
+}
