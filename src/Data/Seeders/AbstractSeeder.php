659010b4a534efc5a0eed0181245b25c976e9de7
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-11-22 14:53:52 +0300

    Create AbstractSeeder.php

---
+++ src/Data/Seeders/AbstractSeeder.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace GetSDK\Shared\Tool\Data\Seeders;
+
+use Illuminate\Database\Seeder as AbstractLaravelSeeder;
+
+/**
+ * AbstractSeeder
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+abstract class AbstractSeeder extends AbstractLaravelSeeder
+{
+
+}
